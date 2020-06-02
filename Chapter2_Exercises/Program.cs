1fc94ac35933fa8b96c2fec48402b37b1c21fa97
Jed Morris <jemorris110@gmail.com>
2020-06-02 08:52:03 -0700

    created "hi" program

---
+++ Chapter2_Exercises/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chapter2_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name +"!");
+        }
+    }
+}
