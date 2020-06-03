a9ee826ee5cedefa9b25e45ee2b2cca40b679a15
Jed Morris <jemorris110@gmail.com>
2020-06-03 09:02:42 -0700

    created project numeric types

---
+++ Numeric_Types/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numeric_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
