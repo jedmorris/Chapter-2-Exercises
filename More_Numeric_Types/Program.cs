0b81a71fae86c4631205b0aedd6861e1fb164396
Jed Morris <jemorris110@gmail.com>
2020-06-03 09:10:30 -0700

    created projects more numeric types and strings

---
+++ More_Numeric_Types/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace More_Numeric_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
