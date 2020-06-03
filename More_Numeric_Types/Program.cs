f369585d5496be9b5fa72c12ff15b8094c4a1653
Jed Morris <jemorris110@gmail.com>
2020-06-03 09:39:40 -0700

    completed more numeric types

--- More_Numeric_Types/Program.cs
+++ More_Numeric_Types/Program.cs
@@ -6,7 +6,15 @@ namespace More_Numeric_Types
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many miles have you driven?");
+            string miles = Console.ReadLine();
+            int milesDriven = int.Parse(miles);
+
+            Console.WriteLine("How many gallons of gas have you consumed?");
+            string gas = Console.ReadLine();
+            int gasConsumed = int.Parse(gas);
+
+            Console.WriteLine($"Your current gas mileage is {milesDriven / gasConsumed}mpg.");
         }
     }
 }
